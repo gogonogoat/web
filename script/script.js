7b913d2746498acb947a59aa776162a3445c4843
gogonogoat <cashmere.1229@gmail.com>
2020-01-07 14:38:43 +0900

    update

---
+++ script/script.js
@@ -0,0 +1,12 @@
+﻿/* IEユーザーご退場*/
+function AntiIE() {
+	let userAgent = window.navigator.userAgent.toLowerCase();
+	if ( userAgent.indexOf( 'msie' ) !== -1 || userAgent.indexOf( 'trident' ) !== -1 ) {
+		location.href='https://www.google.co.jp/chrome/index.html';
+	}
+}
+
+/* DOM構造ロード完了を以てアンチIE発動 */
+document.addEventListener("DOMContentLoaded", function () {
+	AntiIE()
+});
