581738285163aa9a3b1febd30a761c05626e2b6b
gogonogoat <cashmere.1229@gmail.com>
2020-01-07 12:30:46 +0900

    0107update

---
+++ script/swiper_top_01.js
@@ -0,0 +1,19 @@
+﻿/*swiper 動作記述*/
+var swiper = new Swiper('.swiper-container', {
+	slidesPerView: 3,
+	spaceBetween: 460,
+	loop: true,
+	centeredSlides: true,
+	autoplay: {
+		delay: 6000,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
