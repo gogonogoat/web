5dcb8bf3fc21ed609ed5e02cba89ff5301457817
gogonogoat <cashmere.1229@gmail.com>
2020-08-14 13:03:10 +0900

    uri変更

--- portfolio/lib/lytebox3/lytebox.js
+++ portfolio/lib/lytebox3/lytebox.js
@@ -322,7 +322,7 @@ LyteBox.prototype.changeContent = function(imageNum) {
 	this.doc.getElementById('lbNumberDisplay').style.display = 'none';
 	if (this.navType == 2 || this.isLyteframe) {
 		object = this.doc.getElementById('lbNavDisplay');
-		object.innerHTML = '&nbsp;&nbsp;&nbsp;<span id="lbPrev2_Off" style="display: none;" class="' + this.theme + '">&laquo; prev</span><a href="#" id="lbPrev2" class="' + this.theme + '" style="display: none;">&laquo; prev</a> <b id="lbSpacer" class="' + this.theme + '">||</b> <span id="lbNext2_Off" style="display: none;" class="' + this.theme + '">next &raquo;</span><a href="#" id="lbNext2" class="' + this.theme + '" style="display: none;">next &raquo;</a>';
+		object.innerHTML = '   <span id="lbPrev2_Off" style="display: none;" class="' + this.theme + '">« prev</span><a href="#" id="lbPrev2" class="' + this.theme + '" style="display: none;">« prev</a> <b id="lbSpacer" class="' + this.theme + '">||</b> <span id="lbNext2_Off" style="display: none;" class="' + this.theme + '">next »</span><a href="#" id="lbNext2" class="' + this.theme + '" style="display: none;">next »</a>';
 		object.style.display = 'none';
 	}
 	if (this.isLyteframe) {
