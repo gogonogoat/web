9fbd9516589ff3bd606aad1fae67941af79cf5bf
gogonogoat <cashmere.1229@gmail.com>
2021-11-02 13:51:26 +0900

    contentUpdate

---
+++ portfolio/js/retouch.js
@@ -0,0 +1,116 @@
+﻿const product = [
+	{
+	"imageA":"01a.jpg",
+	"imageB":"01b.jpg",
+	"reference_name":"ぱくたそ",
+	"reference_url":"https://www.pakutaso.com/20130926245post-3238.html",
+	"desc1":`
+		題材：どこかで見たような個性的なポーズをキメる男性
+		加工方針：ポーズの元ネタに寄せたコミック調アート
+`,
+	"desc2":`
+		・2階調化を駆使してイラスト調に変換
+		・色収差を追加してアクセントを付ける
+		・ハーフトーン化して背景に配置
+		・ワープテキストを配置して全体のバランス調整
+`
+	},{
+	"imageA":"02a.jpg",
+	"imageB":"02b.jpg",
+	"reference_name":"写真AC",
+	"reference_url":"https://www.photo-ac.com/main/detail/701342",
+	"desc1":`
+		題材：イカ
+		加工方針：とある任天堂作品を意識したポップなストリートアート調
+`,
+	"desc2":`
+		・2階調化を駆使してイラスト(劇画調)に変換
+		・ストリートアートである事を意識してCMYK色空間を用いた配色を用いる
+		・カラーハーフトーンを用いて印刷物らしさを付加
+		・ラフなインクのこぼれを配置してストリートアート調を強調
+		・いかぽっぽを元ネタにポップに仕上げた事を主張するダジャレを配置
+`
+	},{
+	"imageA":"03a.jpg",
+	"imageB":"03b.jpg",
+	"reference_name":"1%の需要に応えた看護師フリー写真素材サイト スキマナース",
+	"reference_url":"https://nurse-web.jp/photo/archives/205",
+	"desc1":`
+		題材：患者とビート板をかじり合う看護師
+		加工方針：せめてビート板以外の物を奪い合ってほしい
+`,
+	"desc2":`
+		・ビート板を形状と陰影だけを残し、テクスチャの差し替えが可能な状態に加工
+		・フリー素材の牛肉をテクスチャとして適用
+`
+	},{
+	"imageA":"04a.jpg",
+	"imageB":"04b.jpg",
+	"reference_name":"ぱくたそ",
+	"reference_url":"https://www.pakutaso.com/20191007289post-23781.html",
+	"desc1":`
+		題材：親の敵のごとくチーズドッグを睨みつける男性
+		加工方針：素材が持つインパクトを残したまま世界観を変更
+`,
+	"desc2":`
+		・男性を背景から切り出す
+		・男性を絵画調に変換
+		・絵画調に変換した背景を用意
+		・背景と男性を合成
+`
+	},{
+	"imageA":"05a.jpg",
+	"imageB":"05b.jpg",
+	"reference_name":"gogonogoat Portfolio",
+	"reference_url":"index.html",
+	"desc1":`
+		題材：このサイトのロゴ
+		加工方針：チープな絵を少ない手間で豪華に
+`,
+	"desc2":`
+		・アンティーク調を意識して背景にレンガを採用
+		・背景に暗めな色調補正を適用
+		・ロゴをレイヤー効果を利用してゴールド調に加工
+`
+	}
+];
+
+const vue = new Vue({
+	el:'#app',
+	data:{
+		product:product
+	},
+	methods:{
+		prefix:function(str){return (str=="") ? "" : "images/retouch/";},
+		toImageUrl:function(str){
+			const prefix = this.prefix(str);
+			const result = prefix + str;
+			return result;
+		},
+		toIcon:function(str){
+			const prefix = this.prefix(str);
+			const extDict = {
+				audacity:".png"
+			};
+			const suffix = (str in extDict) ? extDict[str] : ".svg";
+			const result = (str=="") ? "" : prefix + str + suffix;
+			return result;
+		},
+		descFix:function(str){
+			console.log(str);
+			const replaceList = [
+				[/^\n/,""],
+				[/\n$/,""],
+				[/\n\t{2}/g,"<br>\n"],
+				[/\t{2}/g,""],
+				[/\t/g,"　"]
+			];
+			const replaceReducer = (r,v) => r.replace(v[0],v[1]);
+			const replacefix = replaceList.reduce(replaceReducer,str);
+			
+			console.log(replacefix);
+			const result = replacefix;
+			return result;
+		}
+	},
+});
---
+++ portfolio/js/website.js
@@ -0,0 +1,76 @@
+﻿const product = [
+	{
+	"image":"portfolio.png",
+	"name":"gogonogoat Portfolio",
+	"url":"https://gogonogoat.github.io/web/portfolio",
+	"desc":`
+		持っているスキルを簡単に公開できるようにしたい！
+		でも実名をオンラインに晒すのは怖い。
+		
+		だったらハンドルネーム名義で公開しよう！
+		そのような流れによって、このサイトが作られました。
+`,
+	"skill":`
+		Webサイトの骨組みは、後述のDirty Kitchenを元に微修正を加えた物です。
+		
+		Vue.jsについて学んだ事によって、
+		私はデータと分離された文書構造の良さに気が付きました。
+		それ故にこのサイト全体をを作り直したい意欲が大きくなっています。
+`
+	},{
+	"image":"dirtykitchen.png",
+	"name":"Dirty Kitchen",
+	"url":"https://gogonogoat.github.io/web/portfolio/00_dirty_kitchen",
+	"desc":`
+		架空の飲食店のランディングページです。
+		
+		店舗名が直訳して「汚い台所」である等、飲食店として有りえない要素は
+		実在の飲食店と見られるような誤解を防ぐ意図です。
+`,
+	"skill":`
+		FlexBox等、CSSやHTMLに関する昨今の手法を学び、
+		スキルチェックの目的で制作しました。
+		
+		CSSに試行錯誤の形跡が多く残されており、
+		セレクタの関係性が整頓されていない点が振り返っていて辛い所です。
+`
+	},{
+	"image":"funnypowder.png",
+	"name":"Funny Powder",
+	"url":"https://gogonogoat.github.io/web/portfolio/01_funnypowder/index.html",
+	"desc":`
+		架空の健康食品のランディングページです。
+`,
+	"skill":`
+		レスポンシブデザインについて学ぶ前段階として、
+		スマートフォン向けのWebページを制作する目的で制作しました。
+`
+	}
+];
+const vue = new Vue({
+	el:'#app',
+	data:{
+		product:product
+	},
+	methods:{
+		prefix:function(str){return (str=="") ? "" : "images/website/";},
+		toImageUrl:function(str){
+			const prefix = this.prefix(str);
+			const result = prefix + str;
+			return result;
+		},
+		descFix:function(str){
+			const replaceList = [
+				[/^\n/,""],
+				[/\n$/,""],
+				[/\n\t{2}/g,"<br>\n"],
+				[/\t{2}/g,""],
+				[/\t/g,"　"]
+			];
+			const replaceReducer = (r,v) => r.replace(v[0],v[1]);
+			const replacefix = replaceList.reduce(replaceReducer,str);
+			const result = replacefix;
+			return result;
+		}
+	},
+});
